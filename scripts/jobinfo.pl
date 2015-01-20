ae369a41e44cd259ef7b8b5c41d7ba7225e8aa1b
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-01-20 15:00:46 +0100

    Reindented jobinfo.pl

--- scripts/jobinfo.pl
+++ scripts/jobinfo.pl
@@ -12,14 +12,14 @@ use Getopt::Long;
 use vars qw($USAGE);
 
 BEGIN {
-    
+
     $USAGE =
-	qq{jobinfo.pl - > 
-           -u/--user Limit jobinfo to user
-           -j/--jobId Limit jobinfo to jobID
-           -h/--help Display this help message
-           -v/--version Display version
-          };
+    qq{jobinfo.pl - > 
+    -u/--user Limit jobinfo to user
+    -j/--jobId Limit jobinfo to jobID
+    -h/--help Display this help message
+    -v/--version Display version
+    };
 }
 
 my %line;
@@ -33,10 +33,10 @@ if (defined($ARGV) && $ARGV[0]!~/^-/) { #Collect potential infile - otherwise re
 
 ###User Options
 GetOptions('u|user:s' => \$user,
-	   'j|jobId:s' => \$jobId,
-	   'h|help' => \$help, #Display help text
-	   'v|version' => \$version, #Display version number
-    );
+    'j|jobId:s' => \$jobId,
+    'h|help' => \$help, #Display help text
+    'v|version' => \$version, #Display version number
+);
 
 if($help) {
 
@@ -50,29 +50,29 @@ if($version) {
     print STDOUT "\njobinfo.pl v".$jobInfoVersion, "\n\n";
     exit
 }
-    
+
 print "#";
 for (my $elementCounter=0;$elementCounter<scalar(@orderHeaders);$elementCounter++) {
-    
+
     my $key = \$orderHeaders[$elementCounter];
     if ($$key eq "Name") {
-	
-	printf "%-50s",$$key;
+
+        printf "%-50s",$$key;
     }
     elsif($$key eq "Dependency") {
-	
-	print $$key;
+
+        print $$key;
     }
     else {
-	
-	if ($elementCounter==0) {
 
-	    printf "%-10s",$$key;
-	}
-	else {
+        if ($elementCounter==0) {
 
-	    printf "%-11s",$$key;
-	}
+            printf "%-10s",$$key;
+        }
+        else {
+
+            printf "%-11s",$$key;
+        }
     }
 }
 print "\n";
@@ -82,60 +82,60 @@ my $printStatus = 0;
 while (<>) {    
 
     if ($jobId ne "noInfo") {
-	
-	if($_=~/JobId=(\S+)/) {
-	  
-	    my $currentJobId = $1;
-	    if ($currentJobId eq $jobId) {
-	
-		$printStatus = 1;
-	    }
-	}
+
+        if($_=~/JobId=(\S+)/) {
+
+            my $currentJobId = $1;
+            if ($currentJobId eq $jobId) {
+
+                $printStatus = 1;
+            }
+        }
     }
     elsif ($user ne "noInfo") {
-	
-	if($_=~/UserId=(\w+.\w+)/) {
-	    
-	    my $currentUser = $1;
-	    if ($currentUser eq $user) {
-		
-		$printStatus = 1;
-	    }
-	}
+
+        if($_=~/UserId=(\w+.\w+)/) {
+
+            my $currentUser = $1;
+            if ($currentUser eq $user) {
+
+                $printStatus = 1;
+            }
+        }
     }
     foreach my $key (@orderHeaders) {
-	
-	if($_=~/$key=(\S+)/) { 
-	    
-	    $line{$key} = $1
-	} 
+
+        if($_=~/$key=(\S+)/) { 
+
+            $line{$key} = $1
+        } 
     }
     if($_=~/^\s+$/) {
-	
-	if( ($jobId eq "noInfo") && ($user eq "noInfo") ) {
-	    
-	    $printStatus = 1;
-	}
-	if($printStatus == 1) {
-	    
-	    for (my $elementCounter=0;$elementCounter<scalar(@orderHeaders);$elementCounter++) {
-		
-		my $key = \$orderHeaders[$elementCounter];
-		if ($$key eq "Name") {
-		    
-		    printf "%-50s",$line{$$key};
-		}
-		elsif($$key eq "Dependency") {
-		    
-		    print $line{$$key};
-		}
-		else {
-		    
-		    printf "%-11s",$line{$$key};
-		}
-	    }
-	    print "\n";
-	}
-	$printStatus = 0;  #Reset for next block
+
+        if( ($jobId eq "noInfo") && ($user eq "noInfo") ) {
+
+            $printStatus = 1;
+        }
+        if($printStatus == 1) {
+
+            for (my $elementCounter=0;$elementCounter<scalar(@orderHeaders);$elementCounter++) {
+
+                my $key = \$orderHeaders[$elementCounter];
+                if ($$key eq "Name") {
+
+                    printf "%-50s",$line{$$key};
+                }
+                elsif($$key eq "Dependency") {
+
+                    print $line{$$key};
+                }
+                else {
+
+                    printf "%-11s",$line{$$key};
+                }
+            }
+            print "\n";
+        }
+        $printStatus = 0;  #Reset for next block
     }
 }
