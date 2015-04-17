08c2361f3921cf669fcb81a7a604645c72a89e16
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-04-17 14:57:09 +0200

    Change virtualenv to miniconda

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -17,7 +17,7 @@ for run in ${runs[@]}; do
       echo [${NOW}] [${run}] copy is complete delivery is started > ${UNABASE}${run}/delivery.txt 
   ## addition 20150219 testing auto summary and linking
       FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
-      /home/hiseq.clinical/.virtualenvs/develop/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
+      /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
   ## addition ends
       sbatch /mnt/hds/proj/bioinfo/SCRIPTS/getfastq.batch ${UNABASE}${run}
     fi
