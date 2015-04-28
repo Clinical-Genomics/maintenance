9d43236131aa67b8b10570d3d2c8dcac7fa009e9
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-04-28 21:45:07 +0200

    Add auto trim qxt testing

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -15,11 +15,22 @@ for run in ${runs[@]}; do
     else
       echo [${NOW}] [${run}] copy is complete delivery has not started
       echo [${NOW}] [${run}] copy is complete delivery is started > ${UNABASE}${run}/delivery.txt 
-  ## addition 20150219 testing auto summary and linking
       FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
-      /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
-  ## addition ends
-      sbatch /mnt/hds/proj/bioinfo/SCRIPTS/getfastq.batch ${UNABASE}${run}
+  ## addition 20150428 auto QXT trimming
+      if [ -f ${UNABASE}${run}/trimmed.txt ]; then
+        echo [${NOW}] [${run}] trimming already finished
+      else
+      	/home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} 2>&1 > /mnt/hds/proj/bioinfo/LOG/trimQXT.${FC}.${NOW}.log
+      fi
+      if [ -f ${UNABASE}${run}/trimming.txt ]; then
+        echo [${NOW}] [${run}] trimming is in progress
+      else
+
+  ## addition 20150219 testing auto summary and linking
+        /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
+  ## a  ddition ends
+        sbatch /mnt/hds/proj/bioinfo/SCRIPTS/getfastq.batch ${UNABASE}${run}
+      fi
     fi
 #    if [ -d ${ALIBASE}${run}/Aligned ]; then
 #      echo [${NOW}] [${run}] copy is complete and alignment has already started 
