df54014887e60efb90a73b7cc8f7f61e2710b021
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-05-03 22:23:16 +0200

    Feat checkfornewdemux add trimqxt execution

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -8,38 +8,35 @@ UNABASE=/mnt/hds/proj/bioinfo/DEMUX/
 ALIBASE=/mnt/hds/proj/bioinfo/ALIGN/
 runs=$(ls ${UNABASE})
 for run in ${runs[@]}; do
-  NOW=$(date +"%Y%m%d%H%M%S")
   if [ -f ${UNABASE}${run}/copycomplete.txt ]; then
     if [ -f ${UNABASE}${run}/delivery.txt ]; then
-      echo [${NOW}] [${run}] copy is complete and delivery has already started 
+      log ${run} 'copy is complete and delivery has already started'
     else
-      echo [${NOW}] [${run}] copy is complete delivery has not started
-      echo [${NOW}] [${run}] copy is complete delivery is started > ${UNABASE}${run}/delivery.txt 
-      FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
-  ## addition 20150428 auto QXT trimming
+      log ${run} 'copy is complete delivery has not started'
       if [ -f ${UNABASE}${run}/trimmed.txt ]; then
-        echo [${NOW}] [${run}] trimming already finished
+        log ${run} 'trimming already finished'
       else
-      	/home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} 2>&1 > /mnt/hds/proj/bioinfo/LOG/trimQXT.${FC}.${NOW}.log
+        NOW=$(date +"%Y%m%d%H%M%S")
+      	/home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} 2>&1 > ${UNABASE}${run}/trimQXT.${NOW}.log
       fi
       if [ -f ${UNABASE}${run}/trimming.txt ]; then
-        echo [${NOW}] [${run}] trimming is in progress
+        log ${run} 'trimming is in progress'
       else
-
-  ## addition 20150219 testing auto summary and linking
+        log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt 
+        FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
+        NOW=$(date +"%Y%m%d%H%M%S")
         /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
-  ## a  ddition ends
         sbatch /mnt/hds/proj/bioinfo/SCRIPTS/getfastq.batch ${UNABASE}${run}
       fi
     fi
 #    if [ -d ${ALIBASE}${run}/Aligned ]; then
-#      echo [${NOW}] [${run}] copy is complete and alignment has already started 
+#      log [${run}] copy is complete and alignment has already started 
 #    else
-#      echo [${NOW}] [${run}] copy is complete alignment has not started
+#      log [${run}] copy is complete alignment has not started
 #      sbatch /mnt/hds/proj/bioinfo/SCRIPTS/align.batch ${UNABASE}${run}
 #    fi
   else
-    echo [${NOW}] [${run}] is not yet completely copied
+    log ${run} 'is not yet completely copied'
   fi
 done
 
