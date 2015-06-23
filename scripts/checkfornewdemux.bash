fed7dc020f0a109042ffc48b24f6937fb7a768aa
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-06-23 11:08:11 +0200

    Fix logdir for createfastqlinks in checkfornewdemux

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -25,7 +25,7 @@ for run in ${runs[@]}; do
         log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt 
         FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
         NOW=$(date +"%Y%m%d%H%M%S")
-        /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/getsamplesummary.py ${FC} 2>&1 > /mnt/hds/proj/bioinfo/LOG/samplesum.${FC}.${NOW}.log
+        /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/createfastqlinks.py ${FC} 2>&1 > ${UNABASE}${run}/createfastqlinks.${FC}.${NOW}.log
         sbatch /mnt/hds/proj/bioinfo/SCRIPTS/getfastq.batch ${UNABASE}${run}
       fi
     fi
