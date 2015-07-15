a3ca226972ea867673e46d1999f00a605ddfb546
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-15 11:00:16 +0200

    Add date to cleanup_scratch

--- scripts/cleanup_scratch.bash
+++ scripts/cleanup_scratch.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 VERSION=0.2.1
-echo ${VERSION}
+NOW=$(date +"%Y%m%d%H%M%S")
+echo "[$NOW] ${VERSION}"
 
 # get all job names
 JOBNAMES=( `squeue --noheader --format=%i` )
 
 # create batch script
-BATCHSCRIPT=rm_scratch.bash
+BATCHSCRIPT=/mnt/hds/proj/bioinfo/SCRIPTS/rm_scratch.bash
 
 # submit a batch to each node, deleting all but jobnames dirs in scratch
-NOW=$(date +"%Y%m%d%H%M%S")
 for i in `seq 1 22`; do
     NODENAME=compute-0-$i;
     echo sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
