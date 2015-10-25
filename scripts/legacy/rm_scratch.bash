a7c39f525eb035181108b4bb0f5fb8a9b96b83c5
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-10-25 21:35:26 +0100

    Moved rm_scratch to legacy

---
+++ scripts/legacy/rm_scratch.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION=0.2.1
+echo ${VERSION}
+
+STARTDIR=/scratch
+
+du -sh ${STARTDIR}
+
+for d in `ls $STARTDIR`; do
+    if [[ ${@#} > 0 ]]; then
+        if [[ "${@} " =~ "$d " ]]; then
+            continue
+        fi
+    fi
+    echo chmod -R a+w "$STARTDIR/$d"
+    chmod -R a+w "$STARTDIR/$d"
+    echo rm -rf "$STARTDIR/$d"
+    rm -rf "$STARTDIR/$d"
+done
+
+du -sh ${STARTDIR}
