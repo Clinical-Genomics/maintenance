f52df66d31f533e6de734e646e2c46b577d69e75
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-10-25 21:32:55 +0100

    Feat use rensa_scratch as removal scripts

--- scripts/cleanup_scratch.bash
+++ scripts/cleanup_scratch.bash
@@ -7,12 +7,12 @@ echo ${VERSION}
 JOBNAMES=( `squeue --noheader --format=%i` )
 
 # create batch script
-BATCHSCRIPT=rm_scratch.bash
+BATCHSCRIPT=/usr/local/bin/rensa_scratch
 
 # submit a batch to each node, deleting all but jobnames dirs in scratch
 NOW=$(date +"%Y%m%d%H%M%S")
 for i in `seq 1 22`; do
     NODENAME=compute-0-$i;
     echo sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
-    sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
+    sbatch -c 16 -N 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
 done
