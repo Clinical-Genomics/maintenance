a598c1fa1eae5056f563dde903be5f0878cc45ba
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-12-10 13:39:18 +0100

    Bump version: 0.3.0 → 1.0.0

--- scripts/rm_scratch_all.bash
+++ scripts/rm_scratch_all.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.3.0
+VERSION=1.0.0
 NOW=$(date +"%Y%m%d%H%M%S")
 echo "[$NOW] ${VERSION}"
 
@@ -8,11 +8,11 @@ echo "[$NOW] ${VERSION}"
 JOBNAMES=( `squeue --noheader --format=%i` )
 
 # create batch script
-BATCHSCRIPT=/usr/local/bin/rensa_scratch
+BATCHSCRIPT=/mnt/hds/proj/bioinfo/SCRIPTS/rm_scratch_node.batch
 
 # submit a batch to each node, deleting all but jobnames dirs in scratch
 for i in `seq 1 22`; do
     NODENAME=compute-0-$i;
     echo sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
-    sbatch -c 16 -N 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
+    sbatch -c 1 -A prod001 -t '00:15:00' --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
 done
