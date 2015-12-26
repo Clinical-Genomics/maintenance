1de3e5252404147c8e749b85f8c51d4722d07066
kenny billiau <kenny.billiau@scilifelab.se>
2015-12-26 20:19:25 +0100

    Feat exome/genome RemoveRedundantFiles

--- scripts/RemoveReduntantFiles.sh
+++ scripts/RemoveReduntantFiles.sh
@@ -2,14 +2,21 @@
 
 # will go through all finished analysis and execute the RemoveReduntantFiles.sh script
 
-FINISHED=$(grep 'AnalysisRunStatus: Finished' -l */*/genomes/*/*_qc_sampleInfo.yaml)
+MIP_DIR=$1
 
-for RUN in ${FINISHED}; do
-    ls -l ${RUN}
-
-    CUST=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $1 }')
-    FAMILY=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $2 }')
-
-    echo "bash ${CUST}/${FAMILY}/genomes_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh"
-    bash ${CUST}/${FAMILY}/genomes_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh
+OLDPWD=$(pwd)
+cd ${MIP_DIR}
+for ANALYSIS in exomes genomes; do
+    FINISHED=$(grep 'AnalysisRunStatus: Finished' -l */*/${ANALYSIS}/*/*_qc_sampleInfo.yaml)
+    
+    for RUN in ${FINISHED}; do
+        ls -l ${RUN}
+    
+        CUST=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $1 }')
+        FAMILY=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $2 }')
+    
+        echo "bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh"
+        bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh
+    done
 done
+cd ${OLDPWD}
