19f9f36ed8ad2d8526812f677ff27d3bae35cfe5
kenny billiau <kenny.billiau@scilifelab.se>
2016-02-03 12:50:35 +0100

    Feat beautiful logging check_lsync

--- scripts/check_lsync_nas.bash
+++ scripts/check_lsync_nas.bash
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+
+##########
+# PARAMS #
+##########
+
 NASES=(clinical-nas-2 seq-nas-1 seq-nas-2 seq-nas-3 nas-6 nas-7 nas-8 nas-9 nas-10)
 SCRIPT_DIR=$(dirname $0)
 
+#############
+# FUNCTIONS #
+#############
+ 
+log() {
+    NOW=$(date +"%Y%m%d%H%M%S")
+
+    echo [${NOW}] $@
+}
+
+########
+# MAIN #
+########
+
 for NAS in ${NASES[@]}; do
-    echo -n ${NAS}
     ssh ${NAS} ps -ef | grep lsync | grep -q hiseq.bioinfo
     if [[ $? -eq 0 ]]; then
-        echo ' OK!'
+    	log "${NAS} OK!"
     else
-        echo ' FAIL!'
+    	log "${NAS} FAIL!"
         echo "Lsync process stopped in ${NAS}" | mail -s "LSYNC is not running anymore" kenny.billiau@scilifelab.se
     fi
 done
