c78782da0255e7cc31ecb240633377ac686204ee
kenny billiau <kenny.billiau@scilifelab.se>
2016-02-16 21:44:40 +0100

    Fix check_lsync_nases for network fail

--- scripts/check_lsync_nases.bash
+++ scripts/check_lsync_nases.bash
@@ -5,8 +5,8 @@
 # PARAMS #
 ##########
 
-NASES=(clinical-nas-2 seq-nas-1 seq-nas-2 seq-nas-3 nas-6 nas-7 nas-8 nas-9 nas-10)
-SCRIPT_DIR=$(dirname $0)
+NASES=(cinical-nas-2 seq-nas-1 seq-nas-2 seq-nas-3 nas-6 nas-7 nas-8 nas-9 nas-10)
+EMAILS=kenny.billiau@scilifelab.se,emma.sernstad@scilifelab.se,robin.andeer@scilifelab.se,daniel.backman@scilifelab.se
 
 #############
 # FUNCTIONS #
@@ -23,11 +23,24 @@ log() {
 ########
 
 for NAS in ${NASES[@]}; do
-    ssh ${NAS} ps -ef | grep lsync | grep -q hiseq.bioinfo
+    # get the process list on the NAS
+    SSH_PS=$(ssh ${NAS} ps -ef)
+
+    # check if that command succeeded
+    if [[ $? -ne 0 ]]; then
+        # ssh fail, network fail ... try again in next iteration
+    	log "${NAS} Network FAIL!"
+        echo "${NAS} was not reachable" | mail -s "${NAS} was not reachable!" ${EMAILS}
+        continue
+    fi
+
+    # check the process list on the NAS
+    echo "$SSH_PS" | grep lsync | grep -q hiseq.bioinfo
     if [[ $? -eq 0 ]]; then
     	log "${NAS} OK!"
     else
     	log "${NAS} FAIL!"
-        echo "Lsync process stopped in ${NAS}" | mail -s "LSYNC is not running anymore" kenny.billiau@scilifelab.se,emma.sernstad@scilifelab.se,robin.andeer@scilifelab.se,daniel.backman@scilifelab.se
+        echo "Lsync process stopped in ${NAS}" | mail -s "LSYNC is not running anymore" ${EMAILS}
     fi
+    sleep 2
 done
