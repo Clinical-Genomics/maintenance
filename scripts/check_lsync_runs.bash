417f64a959cffac862a485de2f284184803e4a86
kenny billiau <kenny.billiau@scilifelab.se>
2016-02-16 10:02:02 +0100

    Fix logging for check_lsync_runs.bash

--- scripts/check_lsync_runs.bash
+++ scripts/check_lsync_runs.bash
@@ -2,13 +2,26 @@
 
 # checks if a run still syncs
 
+#############
+# FUNCTIONS #
+#############
+ 
+log() {
+    NOW=$(date +"%Y%m%d%H%M%S")
+
+    echo [${NOW}] $@
+}
+
+########
+# MAIN #
+########
+
 RUN_DIR=$1
 for RUN in ${RUN_DIR}/*; do
     # skip non-runs
     if [[ ! -d ${RUN} ]]; then continue; fi
 
-    echo -n ${RUN}
-
+    # if the run is not complete, check if it is still syncing!
     if [[ ! -f ${RUN}/RTAComplete.txt ]]; then
         # fine last file
         LAST_TIMESTAMP=$(find ${RUN} -type f -printf '%T@' | sort -n | tail -1) 
@@ -17,11 +30,11 @@ for RUN in ${RUN_DIR}/*; do
         # if latest file older then 15mins ...
         if [[ $LAST_TIMESTAMP_AGO -gt 1000 ]]; then
             #echo "${RUN} is not syncing anymore!" | mail -s "${RUN} is not running anymore!" kenny.billiau@scilifelab.se,emma.sernstad@scilifelab.se,robin.andeer@scilifelab.se,daniel.backman@scilifelab.se
-            echo ' Sync FAIL!'
+            log "${RUN} Sync FAIL!"
         else
-            echo ' Syncing'
+            log "${RUN} Syncing"
         fi
     else
-        echo ' Finished'
+        log "${RUN} Finished"
     fi
 done
