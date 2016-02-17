1828f301d958ce76bc9cfd9d036bed2c0fbe2345
kenny billiau <kenny.billiau@scilifelab.se>
2016-02-17 14:59:28 +0100

    Fix logging of RemoveRedundantFiles

--- scripts/RemoveReduntantFiles.sh
+++ scripts/RemoveReduntantFiles.sh
@@ -2,7 +2,26 @@
 
 # will go through all finished analysis and execute the RemoveReduntantFiles.sh script
 
+##########
+# PARAMS #
+##########
+
 MIP_DIR=$1
+MAX_AGE=${2-7}
+
+#############
+# FUNCTIONS #
+#############
+ 
+logn() {
+    NOW=$(date +"%Y%m%d%H%M%S")
+
+    echo -n [${NOW}] $@
+}
+
+########
+# MAIN #
+########
 
 OLDPWD=$(pwd)
 cd ${MIP_DIR}
@@ -13,13 +32,20 @@ for ANALYSIS in exomes genomes; do
 
        # only remove the runs that are a week old...
        DAYS_AGO=$(( ( $(date +%s) - $(stat ${RUN} -c %Y) ) / ( 60 * 60 * 24 ) ))
-       echo "${RUN} ran ${DAYS_AGO} days ago"
-       if [[ ${DAYS_AGO} -gt 6 ]]; then
+       logn "${RUN} ran ${DAYS_AGO} days ago"
+       if [[ ${DAYS_AGO} -gt ${MAX_AGE} ]]; then
             CUST=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $1 }')
             FAMILY=$(echo ${RUN} | awk 'BEGIN { FS="/" } { print $2 }')
  
-            echo "bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh"
-            bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh
+            #echo "bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh"
+            bash ${CUST}/${FAMILY}/${ANALYSIS}_scripts/${FAMILY}/bwa/RemoveRedundantFiles_${FAMILY}.*.sh &> /dev/null
+            if [[ $? -eq 0 ]]; then
+                echo ", removing redundant files ..."
+            else
+                echo ", already removed!"
+            fi
+        else
+            echo ", younger than ${MAX_AGE} days! Keeping."
         fi
     done
 done
