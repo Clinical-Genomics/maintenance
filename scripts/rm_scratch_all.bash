17fadfc862d1f54bf83fddbac497d8848ba10556
kenny billiau <kenny.billiau@scilifelab.se>
2016-03-03 14:48:56 +0100

    Fix LOG_DIR as seperate param in rm_scratch_all

--- scripts/rm_scratch_all.bash
+++ scripts/rm_scratch_all.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 VERSION=2.3.8
+LOG_DIR=${1-/mnt/hds/proj/bioinfo/LOG/}
+
+
 NOW=$(date +"%Y%m%d%H%M%S")
 echo "[$NOW] ${VERSION}"
 
@@ -13,6 +16,6 @@ BATCHSCRIPT=/mnt/hds/proj/bioinfo/SCRIPTS/rm_scratch_node.batch
 # submit a batch to each node, deleting all but jobnames dirs in scratch
 for i in `seq 1 22`; do
     NODENAME=compute-0-$i;
-    echo sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
-    sbatch -c 1 -A prod001 -t '00:15:00' --nodelist="$NODENAME" --output=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.log --error=/mnt/hds/proj/bioinfo/LOG/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
+    echo sbatch -c 1 -A prod001 --nodelist="$NODENAME" --output=${LOG_DIR}/scratch-$NODENAME.$NOW.log --error=${LOG_DIR}/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
+    sbatch -c 1 -A prod001 -t '00:15:00' --nodelist="$NODENAME" --output=${LOG_DIR}/scratch-$NODENAME.$NOW.log --error=${LOG_DIR}/scratch-$NODENAME.$NOW.err $BATCHSCRIPT ${JOBNAMES[*]}
 done
